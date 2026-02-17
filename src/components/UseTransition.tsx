663823423a232d7a33c9d69760b76c428f1338a2
ishwork <ishworkhadka70@gmail.com>
2026-02-17 23:42:24 +0200

    update: remove unused import

--- src/components/UseTransition.tsx
+++ src/components/UseTransition.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useTransition } from 'react';
+import { useState, useTransition } from 'react';
 
 function UseTransition() {
   const [isPending, startTransition] = useTransition();
